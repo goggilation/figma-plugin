2e77729d3d014e6b4a4d69eb8614f305ff0c6249
Aleksander Djordjevic <aleksander.k.djordjevic@gmail.com>
2021-04-07 16:26:20 +0200

    Adding all these fine plugins

---
+++ create-boilerplate/code.js
@@ -0,0 +1,34 @@
+figma.showUI(__html__);
+figma.ui.resize(400, 260);
+// Find the current page node inside the document and store it's id
+const firstPageID = figma.root.findOne(node => node.type === "PAGE").id;
+// Setup pages for boilerplate
+const pages = [
+    {
+        name: 'Goal',
+    },
+    {
+        name: 'Explore'
+    },
+    {
+        name: 'Hypothesis'
+    },
+    {
+        name: 'Insights'
+    },
+];
+//Set name of current first page to the first page in boilerplate
+figma.root.findChild(n => n.id === firstPageID).name = pages[0].name;
+//Add all other pages to the document
+pages.map((page, index) => {
+    if (index != 0) {
+        const newPage = figma.createPage();
+        newPage.name = page.name;
+    }
+    return null;
+});
+figma.ui.onmessage = msg => {
+    if (msg.type === 'close') {
+        figma.closePlugin();
+    }
+};
